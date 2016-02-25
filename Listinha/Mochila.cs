7137cd012cf25562b4afd4db736bfc24590d953d
Emanuel Umbelino <emanuel.umbelino@hotmai.com>
2016-02-25 10:13:00 -0300

    Version 0.2
    - Interface Alpha

---
+++ Listinha/Mochila.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listinha
+{
+    class Mochila
+    {
+        #region Property
+        private int posicao;
+        private int tamanho;
+        private Mochila mochilinha;
+        private Mochila mochilona;
+        #endregion
+
+        #region Get/Set
+        public int Position() { return posicao; }
+        public void Position(int i) { posicao = i; }
+        public int Index() { return tamanho; }
+        public void Index(int i) { tamanho = i; }
+        public Mochila Next() { return mochilinha; }
+        public void Next(Mochila i) { mochilinha = i; }
+        public Mochila Previous() { return mochilona; }
+        public void Previous(Mochila i) { mochilona = i; }
+        #endregion
+    }
+}
